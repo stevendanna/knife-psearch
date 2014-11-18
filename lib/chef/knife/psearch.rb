b81dfd552d999dadfcc155c329b8e5f4c3aa82a8
Steven Danna <steve@opscode.com>
2014-11-18 20:45:00 +0000

    Make name=>key mapping optional, add -i option
    Previously, knife-psearch required a user-supplied mapping of
    attribute names to a key path, similar to what the underlying API
    requires.  This commit makes that mapping optional by automatically
    constructing a partial search request based on one of the following:
    
    - Any `-a ATTR` flags passed on the command line,
    - The `--id-only` option passed on the command line,
    - A default set of attributes for nodes to mimic knife-search
    
    Otherwise, it will fall back to doing a full search.

--- lib/chef/knife/psearch.rb
+++ lib/chef/knife/psearch.rb
@@ -5,10 +5,16 @@
 #
 # This plugin is not yes officially supported by Opscode
 
+require 'chef/knife'
+require 'chef/knife/core/node_presenter'
+
 class Psearch < Chef::Knife
-  banner "knife psearch INDEX SEARCH NAME=DESIRED_KEY_PATH,[NAME=DESIRED_KEY_PATH]"
+  banner "knife psearch INDEX SEARCH [NAME=DESIRED_KEY_PATH,[NAME=DESIRED_KEY_PATH]]"
+
+  include ::Chef::Knife::Core::MultiAttributeReturnOption
 
   deps do
+    require 'chef/node'
     require 'chef/search/partial_search'
   end
 
@@ -25,6 +31,11 @@ class Psearch < Chef::Knife
   :default => 0,
   :proc => lambda { |i| i.to_i }
 
+  option :id_only,
+  :short => "-i",
+  :long => "--id-only",
+  :description => "Show only the ID of matching objects"
+
   option :rows,
   :short => "-R INT",
   :long => "--rows INT",
@@ -32,20 +43,66 @@ class Psearch < Chef::Knife
   :default => 1000,
   :proc => lambda { |i| i.to_i }
 
-  option :attribute,
-  :short => "-a ATTR",
-  :long => "--attribute ATTR",
-  :description => "Show only one attribute"
+  DEFAULT_NODE_HASH = {
+    "name" => ["name"],
+    "chef_environment" => ["chef_environment"],
+    "fqdn" => ["fqdn"],
+    "ipaddress" => ["ipaddress"],
+    "run_list" => ["run_list"],
+    "roles" => ["roles"],
+    "recipes" => ["recipes"],
+    "platform" => ["platform"],
+    "tags" => ["tags"]
+  }
+
+  # "id" will be used by the generic presenter automatically when
+  # config[:id_only] is true
+  ID_ONLY_HASH = {
+    "id" => ["name"]
+  }
 
   def run
     @index, @search, *@keys = @name_args
+    @inflate_nodes = false
+
     args_hash = {}
-    args_hash[:keys] = build_key_hash
+    args_hash[:keys] = if config[:id_only]
+                         ID_ONLY_HASH
+                       elsif ! @keys.empty?
+                         build_key_hash
+                       elsif ! config[:attribute].nil? && ! config[:attribute].empty?
+                         # config[:attribute] comes from the -a option,
+                         # which is provided by Knife::Core::MultiAttributeReturnOption
+                         build_key_hash_from_attrs(config[:attribute])
+                       elsif @index == "node"
+                         DEFAULT_NODE_HASH
+                       else
+                         nil
+                       end
+
+    # Create output similar to knife-search
+    # in the default case by creating Chef::Node objects
+    # and using the node presenter
+    if args_hash[:keys] == DEFAULT_NODE_HASH && @index == 'node'
+      Chef::Log.debug("Using NodePresenter for output")
+      ui.use_presenter ::Chef::Knife::Core::NodePresenter
+      @inflate_nodes = true
+    end
+
     args_hash[:sort] = config[:sort]
     args_hash[:start] = config[:start]
     args_hash[:rows] = config[:rows]
     results = Chef::PartialSearch.new.search(@index, @search, args_hash)
-    ui.output ui.format_for_display(results.first)
+    print_results(results.first)
+  end
+
+  def build_key_hash_from_attrs(attrs)
+    key_hash = {}
+    attrs.each do |a|
+      key_hash[a] = a.split(".")
+    end
+    key_hash["id"] = [ "name" ] unless key_hash.has_key?("name")
+    key_hash
   end
 
   def build_key_hash
@@ -59,5 +116,20 @@ class Psearch < Chef::Knife
     # are usually not what we want.
     key_hash["name"] = [ "name" ] unless key_hash.has_key?("name")
     key_hash
+  end
+
+  def print_results(items)
+    items.each do |res|
+      res = create_node(res) if @inflate_nodes
+      ui.output ui.format_for_display(res)
+      puts "\n" if ! config[:id_only]
+    end
+  end
+
+  def create_node(node_data)
+    node_data['attributes'] = node_data.reject do |key, value|
+      ["name", "chef_environment", "run_list"].include?(key)
+    end
+    Chef::Node.json_create(node_data)
   end
 end
